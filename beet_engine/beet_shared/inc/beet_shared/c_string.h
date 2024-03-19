a8e92b9d87d6909a9121f6991f5c0923f334e3c8
BeardyKing <39307332+BeardyKing@users.noreply.github.com>
2024-03-19 21:50:01 +0000

    [CORE] Added reverse c_str search & c_str n equals
    I needed a reverse string search for something unrelated to this project, but I can imagine various use cases for this in the future

--- beet_engine/beet_shared/inc/beet_shared/c_string.h
+++ beet_engine/beet_shared/inc/beet_shared/c_string.h
@@ -1,7 +1,13 @@
 #ifndef BEETROOT_C_STRING_H
 #define BEETROOT_C_STRING_H
 
+#include <cstdint>
+
 bool c_str_empty(const char *inStr);
 bool c_str_equal(const char *lhs, const char *rhs);
+bool c_str_n_equal(const char *lhs, const char *rhs, size_t count);
+
+const char *c_str_search_reverse(const char *src, const char *subStr);
+const char *c_str_n_search_reverse(const char *src, int32_t srcLen, const char *subStr, int32_t subStrLen);
 
 #endif //BEETROOT_C_STRING_H
--- beet_engine/beet_shared/src/c_string.cpp
+++ beet_engine/beet_shared/src/c_string.cpp
@@ -8,3 +8,27 @@ bool c_str_empty(const char *inStr) {
 bool c_str_equal(const char *lhs, const char *rhs) {
     return (strcmp(lhs, rhs) == 0);
 }
+
+bool c_str_n_equal(const char *lhs, const char *rhs, const size_t count) {
+    return (strncmp(lhs, rhs, count) == 0);
+}
+
+const char *c_str_n_search_reverse(const char *src, const int32_t srcLen, const char *subStr, const int32_t subStrLen) {
+    const int32_t itrStart = srcLen - subStrLen;
+
+    for (int32_t i = itrStart; i >= 0; i--) {
+        if (c_str_n_equal(&src[i], subStr, subStrLen)) {
+            return &src[i];
+        }
+    }
+    return nullptr;
+}
+
+const char *c_str_search_reverse(const char *src, const char *subStr) {
+    const int32_t srcLen = (int32_t) strlen(src);
+    const int32_t subStrLen = (int32_t) strlen(subStr);
+    const int32_t itrStart = srcLen - subStrLen;
+
+    return c_str_n_search_reverse(src, srcLen, subStr, subStrLen);
+}
+
